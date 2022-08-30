d847ae2a54304f0c819cbe1fd30f9ef12017ac71
Ramon Munoz <ramonmunoz1027@gmail.com>
2022-08-29 21:49:30 -0700

    Dev :: added console logs

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,8 @@
 import Meta from '../components/meta'
+import { consoleLog } from '../uitls'
 
 export default function Layout({ preview, children }) {
+  consoleLog()
   return (
     <>
       <Meta />
